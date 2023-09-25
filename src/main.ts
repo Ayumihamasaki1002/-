f964b508e19db5fa3ad65bfb05186c1a2179c4fd
Mr.Lin <10974595+mrlin0211@user.noreply.gitee.com>
2023-09-25 17:14:59 +0800

    突破懒加载实现

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ import router from './router'
 // 引入初始化样式文件
 import '@/styles/common.scss'
 
+import { useIntersectionObserver } from '@vueuse/core'
+
 
 // 测试接口函数
 // import { getCategory } from '@/apis/testAPI'
@@ -21,3 +23,32 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+
+
+// 定义懒加载插件
+
+
+// export const lazyPlugin = {
+//   install (app) {
+    // 懒加载指令逻辑
+    app.directive('img-lazy', {
+      mounted (el, binding) {
+        // el: 指令绑定的那个元素 img
+        // binding: binding.value  指令等于号后面绑定的表达式的值  图片url
+        console.log(el, binding.value)
+        const { stop } = useIntersectionObserver(
+          el,
+          ([{ isIntersecting }]) => {
+            console.log(isIntersecting)
+            if (isIntersecting) {
+              // 进入视口区域
+              el.src = binding.value
+              stop()
+            }
+          },
+        )
+      }
+    })
+//   }
+// }
