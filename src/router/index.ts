341e94bee9c062da9ef1f9fe752c2eaae58c3ba3
Mr.Lin <10974595+mrlin0211@user.noreply.gitee.com>
2023-09-25 22:57:05 +0800

    路由配置

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,36 +3,37 @@
 
 
 
-import { createRouter, createWebHistory } from 'vue-router'
-import login from '@/views/login/index.vue'
-import layout from '@/views/layout/index.vue'
-import home from '@/views/home/index.vue'
-import category from '@/views/category/index.vue'
-
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Layout from '@/views/Layout/index.vue'
+import Home from '@/views/Home/index.vue'
+import Category from '@/views/Category/index.vue'
+import Login from '@/views/Login/index.vue'
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path:'/',
-      component: layout,
-      children:[
+      path: '/',
+      name: 'layout',
+      component: Layout,
+      children: [
         {
-          path:'',
-          component:home
-        
+          path: '',
+          name: 'home',
+          component: Home
         },
         {
-          path:'/category',
-          component:category
-
+          path: 'category/:id',
+          name: 'category',
+          component: Category
         }
       ]
-
     },
     {
-      path:'/login',
-      component: login
-    }
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+
   ]
 })
 
